751109c5d081155328d48452e3b889a145c32712
gghome <gguman@rambler.ru>
2020-12-06 13:57:41 +0300

    complete func check_gradient

--- assignments/assignment1/gradient_check.py
+++ assignments/assignment1/gradient_check.py
@@ -15,7 +15,7 @@ def check_gradient(f, x, delta=1e-5, tol = 1e-4):
     Return:
       bool indicating whether gradients match or not
     '''
-    
+    #import ipdb; ipdb.set_trace()
     assert isinstance(x, np.ndarray)
     assert x.dtype == np.float
     
@@ -25,7 +25,7 @@ def check_gradient(f, x, delta=1e-5, tol = 1e-4):
 
     assert analytic_grad.shape == x.shape
     analytic_grad = analytic_grad.copy()
-
+    
     # We will go through every dimension of x and compute numeric
     # derivative for it
     it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
@@ -35,6 +35,16 @@ def check_gradient(f, x, delta=1e-5, tol = 1e-4):
         numeric_grad_at_ix = 0
 
         # TODO compute value of numeric gradient of f to idx
+        
+        #x = orig_x.copy()
+        #x[ix] = x[ix] + delta
+        #numeric_grad_at_ix = (f(x)[0] - f(orig_x)[0])/ delta
+        
+        zeros = np.zeros(x.shape)
+        zeros[ix] = delta
+        numeric_grad_at_ix = (f(np.add(x, zeros))[0] - f(np.subtract(x, zeros))[0])/ (2 * delta)
+        
+        
         if not np.isclose(numeric_grad_at_ix, analytic_grad_at_ix, tol):
             print("Gradients are different at %s. Analytic: %2.5f, Numeric: %2.5f" % (ix, analytic_grad_at_ix, numeric_grad_at_ix))
             return False
